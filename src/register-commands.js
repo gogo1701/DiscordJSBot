65b6d17812f3f7906b33aaaee1576ede8c2df902
Georgi <30569429+gogo1701@users.noreply.github.com>
2024-10-17 21:40:52 +0300

    Initial Commit

---
+++ src/register-commands.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const {REST, Routes, ApplicationCommandOptionType} = require('discord.js'); 
+
+const commands = [
+    {
+        name: 'hey',
+        description: 'Replies with hey!',
+    },
+    {
+        name: 'ping',
+        description: 'Pong!',  
+    },
+    {
+        name: 'add',
+        description: 'Adds two numbers.',
+        options: [
+            {
+                name: 'first-number',
+                description: 'The first number.',
+                type: ApplicationCommandOptionType.Number,
+                required: true,
+            },
+            {
+                name: 'second-number',
+                description: 'The second number.',
+                type: ApplicationCommandOptionType.Number,
+                required: true,
+            },
+        ]
+    }
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log("Registering slash commands...");
+
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            {body: commands}
+        )
+
+        console.log("Slash commands were registered correctly!")
+    } catch (error) {
+        console.log(`There was an error: ${error}`)
+    }
+})();
